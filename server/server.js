ee8ba3ea7dc59d013b13c00014576f9ad95b8eb4
Cameron Malloy <cmalloy@berkeley.edu>
2018-07-14 18:54:54 -0700

    building back up

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('scoket.io');
+
+const LambdaGame = require('./lambda-game');
+
+const app = express();
+
+const clientPath = `${__dirname}/../client`;
+
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+let matchMap = new Map();
+
+io.on('connection', (sock) => {
+    
+    sock.on('secret', (key) => {
+        if (matchMap.has(key)) {
+            firstSock = matchMap.get(key);
+            matchMap.delete(key);
+            new LambdaGame(firstSock, sock);
+        }
+        else {
+            matchMap.set(key, sock);
+            sock.emit('message', 'Waiting for opponent');
+        }
+    });
+})
+
+server.on('error', (err) => {
+    console.error('Server error:', err);
+});
+
+server.listen(8080, () => {
+    console.log('LG started on 8080');
+});
