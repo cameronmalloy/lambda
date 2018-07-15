e448f772f0c872238004a2f9027b6ea86ab24924
Cameron Malloy <cmalloy@berkeley.edu>
2018-07-15 00:00:26 -0700

    Added postgres

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const { Pool } = require('pg');
 
 const LambdaGame = require('./lambda-game');
 
@@ -45,4 +46,21 @@ const port = process.env.PORT || 3000;/*8080;*/
 
 server.listen(port, () => {
     console.log('LG started on 8080');
+});
+
+const pool = new Pool({
+  connectionString: 'process.env.postgres://cggcktywvhqxxs:043a113824ae68699347c04572cd0864fe79eb7284e09a72e3b226dc58b99122@ec2-54-227-240-7.compute-1.amazonaws.com:5432/d6sdpfkqvnu2j3',
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
